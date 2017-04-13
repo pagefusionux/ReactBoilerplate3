b486c69c2cc1ad9325a36c8f8adef62f2b933753
Chris Davis <chris.davis@clearlink.com>
2017-04-13 12:50:55 -0600

    Tested node server.js to check successful execution.

--- app/app.jsx
+++ app/app.jsx
@@ -8,9 +8,9 @@ const ReactDOM = require('react-dom');
 const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 // route/page aliases to be configured in webpack.config.js
-const Main = require('Main');
-const Timer = require('Timer');
-const Countdown = require('Countdown');
+//const Main = require('Main');
+//const Timer = require('Timer');
+//const Countdown = require('Countdown');
 
 // load Foundation (using style and css loaders)
 //require('style!css!foundation-sites/dist/css/foundation.min.css');
